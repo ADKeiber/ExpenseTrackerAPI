8af02ffb157ccbfef832841e7584367dd633ad8b
Austin Keiber <adkeiber@gmail.com>
2024-11-25 23:29:51 -0500

    add error handling and user interface

---
+++ src/main/java/com/adk/expensetracker/service/UserService.java
@@ -0,0 +1,37 @@
+package com.adk.expensetracker.service;
+
+import com.adk.expensetracker.model.User;
+
+public class UserService implements IUserService {
+
+	@Override
+	public User createUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User readUser(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User updateUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User deleteUserById(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User deleteUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
